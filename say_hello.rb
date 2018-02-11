66d369fbcc73ce7b438186dcfca3174c075e8fc6
Michael Blank <michael.blank@wework.com>
2018-02-11 18:36:50 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -5,6 +5,13 @@ persons_name = "Kent Beck"
 def say_hello(name)
     puts "Hello #{name}!"
   end
+
+def say_hello()
+    puts "Hello Ruby Programmer!"
+    
+  end
   
  say_hello(name)
+ say_hello()
+ 
  
